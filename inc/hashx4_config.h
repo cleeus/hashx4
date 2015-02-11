79c39647c4714b1a532eda472e6bb4cb8b372ec1
Kai Dietrich <mail@cleeus.de>
2015-02-11 13:36:38 +0100

    add: better auto config

--- inc/hashx4_config.h
+++ inc/hashx4_config.h
@@ -7,15 +7,33 @@
 #   define HX4_HAS_MMX 1
 # else
 #   define HX4_HAS_MMX 0
-#endif
+# endif
 
 # define HX4_HAS_SSE2 1
 # define HX4_HAS_SSSE3 1
+
+#elif defined(__GNUC__)
+
+# ifdef __MMX__
+#   define HX4_HAS_MMX 1
+# else
+#   define HX4_HAS_MMX 0
+# endif
+
+# ifdef __SSE2__
+#   define HX4_HAS_SSE2 1
+# else
+#   define HX4_HAS_SSE2 0
+# endif
+
+# ifdef __SSSE3__
+#   define HX4_HAS_SSSE3 1
+# else
+#   define HX4_HAS_SSSE3 0
+# endif
 
 #else
-# define HX4_HAS_MMX 1
-# define HX4_HAS_SSE2 1
-# define HX4_HAS_SSSE3 1
+# error platform auto config not implemented for this compiler
 #endif
 
 #endif
